000c608da4aea70fb13c8d91629be136158a7f1e
WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
2024-10-18 12:04:04 -0400

    s

--- index.js
+++ index.js
@@ -6,6 +6,44 @@ const DOMSelectors = {
   form: document.querySelector("form"),
   container: document.querySelector(".container"),
 };
+let n = 1;
+function addCard() {
+  const RValue = document.querySelector("#red").value;
+  const GValue = document.querySelector("#green").value;
+  const BValue = document.querySelector("#blue").value;
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card" id="card-${n}"><h2 class="card-header">Red - ${RValue}, Green - ${GValue}, Blue - ${BValue}</h2></div>`
+  );
+  if (
+    Number.isInteger(RValue) &&
+    Number.isInteger(GValue) &&
+    Number.isInteger(BValue)
+  );
+  {
+    console.log(n);
+    document.querySelector(
+      `#card-${n}`
+    ).style.backgroundColor = `rgb(${RValue}, ${GValue}, ${BValue})`;
+  }
+  n += 1;
+}
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  DOMSelectors.button.style.backgroundColor = "white";
+  setTimeout(function () {
+    DOMSelectors.button.style.backgroundColor = "lightgrey";
+  }, 50);
+  addCard();
+});
+/* const DOMSelectors = {
+  header: document.querySelector("h1"),
+  button: document.querySelector("#btn"),
+  items: document.querySelectorAll("li"),
+  cardHeader: document.querySelector(".card-header"),
+  form: document.querySelector("form"),
+  container: document.querySelector(".container"),
+};
 function addCard() {
   let n = 0;
   const RValue = document.querySelector("#red").value;
@@ -23,7 +61,7 @@ function addCard() {
   {
     document.querySelector(
       `#${n}`
-    ).backgroundColor = `rgb(${RValue}, ${GValue}, ${BValue})`;
+    ).style.backgroundColor = `rgb(${RValue}, ${GValue}, ${BValue})`;
   }
 }
 DOMSelectors.form.addEventListener("submit", function (event) {
@@ -34,4 +72,4 @@ DOMSelectors.form.addEventListener("submit", function (event) {
   }, 50);
   n += 1;
   addCard();
-});
+}); */
