d7d19c4673aeff7cb4e791043f67dd8c7800b02a
WilsonD09 <wilsond34@nycstudents.net>
2024-10-18 11:25:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,32 @@ const DOMSelectors = {
   form: document.querySelector("form"),
   container: document.querySelector(".container"),
 };
-
+function addCard() {
+  let n = 0;
+  const RValue = document.querySelector("#red").value;
+  const GValue = document.querySelector("#green").value;
+  const BValue = document.querySelector("#blue").value;
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card" id="${n}"><h2 class="card-header">${RValue}, ${GValue}, ${BValue}</h2></div>`
+  );
+  if (
+    Number.isInteger(RValue) &&
+    Number.isInteger(GValue) &&
+    Number.isInteger(BValue)
+  );
+  {
+    document.querySelector(
+      `#${n}`
+    ).backgroundColor = `rgb(${RValue}, ${GValue}, ${BValue})`;
+  }
+}
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   DOMSelectors.button.style.backgroundColor = "white";
   setTimeout(function () {
     DOMSelectors.button.style.backgroundColor = "lightgrey";
   }, 50);
-  const RValue = document.querySelector("#red").value;
-  const GValue = document.querySelector("#green").value;
-  const BValue = document.querySelector("#blue").value;
-  DOMSelectors.container.insertAdjacentHTML(
-    "beforeend",
-    `<div class="card"><h2 class="card-header">${RValue}, ${GValue}, ${BValue}</h2></div>`
-  );
-  if (RValue.isInteger() && GValue.isInteger() && BValue.isInteger());
-  {
-    document.body.style.backgroundColor = (RValue, GValue, BValue);
-  }
+  n += 1;
+  addCard();
 });
